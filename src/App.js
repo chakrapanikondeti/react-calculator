9a360a8f14a582e19d91c4d35e817db45c2a8eaf
chakrapanikondeti <mailpostcard9@gmail.com>
2023-09-11 15:13:54 +0530

    react-calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Keypad from "./Keypad";
+import "./index.css";
 
 function App() {
+  let [input, setInput] = useState("");
+
+  function handleClick(value) {
+    setInput(input + value);
+  }
+  function calculate(value) {
+    let outputVal = eval(input);
+    setInput(outputVal);
+  }
+  function handleClear() {
+    setInput("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React Calculator</h1>
+      <div className="calculator">
+        <input type="text" value={input} className="output" />
+        <Keypad
+          handleClick={handleClick}
+          handleClear={handleClear}
+          calculate={calculate}
+        />
+      </div>
     </div>
   );
 }
